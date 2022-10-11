afb32a3256d4f1aa13e102a064d1a62e4d1b54f2
Lukas Leeb <git@lukasl.dev>
2022-10-11 19:53:31 +0200

    feat: add ObjectClass type

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
+export * from './objects'
 export * from './filter'
 export * from './client'
 export * from './search-result'
---
+++ lib/objects/object-class.ts
@@ -0,0 +1,8 @@
+export type ObjectClass =
+  | 'group'
+  | 'organizationalUnit'
+  | 'group'
+  | 'posixGroup'
+  | 'person'
+  | 'organizationalPerson'
+  | 'inetOrgPerson'
